35daaf5bb030c0f509334090315ff73b3ce674f8
Ben Rosen <benjamin.p.rosen@gmail.com>
2023-09-07 12:46:06 -0400

    fix position system so it gracefully handles things that used to cause it to throw

--- client/engine/positioned-system.ts
+++ client/engine/positioned-system.ts
@@ -19,28 +19,30 @@ new System<Positioned>(
 
     const physicalEntity = Physics.getEntityById(entityId);
 
-    if (physicalEntity === undefined) {
-      throw new Error(
-        `Entity with id ${entityId} does not exist in physics engine.`,
-      );
-    }
-
-    if (mass === 0) {
-      physicalEntity.position.x = x;
-      physicalEntity.position.y = y;
-      physicalEntity.position.z = z;
+    if (physicalEntity) {
+      if (mass === 0) {
+        physicalEntity.position.x = x;
+        physicalEntity.position.y = y;
+        physicalEntity.position.z = z;
+      } else {
+        console.warn(
+          `Entity with id ${entityId} has non-zero mass but no associated physical entity.`,
+        );
+      }
     }
 
     const graphicalEntity = Graphics.getEntityById(entityId);
 
-    if (graphicalEntity === undefined) {
-      throw new Error(
-        `Entity with id ${entityId} does not exist in graphics engine.`,
-      );
+    if (graphicalEntity) {
+      if (physicalEntity) {
+        graphicalEntity.position.x = physicalEntity.position.x;
+        graphicalEntity.position.y = physicalEntity.position.y;
+        graphicalEntity.position.z = physicalEntity.position.z;
+      } else {
+        graphicalEntity.position.x = x;
+        graphicalEntity.position.y = y;
+        graphicalEntity.position.z = z;
+      }
     }
-
-    graphicalEntity.position.x = physicalEntity.position.x;
-    graphicalEntity.position.y = physicalEntity.position.y;
-    graphicalEntity.position.z = physicalEntity.position.z;
   },
 );
