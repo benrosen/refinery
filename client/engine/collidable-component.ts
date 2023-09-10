4af14ce77ab97b585b62af678b082a578f990766
Ben Rosen <benjamin.p.rosen@gmail.com>
2023-09-10 17:13:19 -0400

    remove typeguard class property

--- client/engine/collidable-component.ts
+++ client/engine/collidable-component.ts
@@ -4,8 +4,6 @@ import { Component } from "./component";
 export class CollidableComponent extends Component<Collidable> {
   public static readonly type = "collidable";
 
-  public readonly isCollidable = true;
-
   constructor(entityId: string) {
     super(CollidableComponent.type, entityId, {
       isCollidable: true,
